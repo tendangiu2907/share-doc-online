bbb187fa662c752a574de34a8cb898e945a0923c
tendangiu <imngochue0060@gmail.com>
2024-12-25 23:51:14 +0700

    add document history

--- src/main/java/com/share_doc/sharing_document/entity/User.java
+++ src/main/java/com/share_doc/sharing_document/entity/User.java
@@ -2,10 +2,9 @@ package com.share_doc.sharing_document.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.*;
-
 import java.util.ArrayList;
 import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "user")
@@ -15,33 +14,41 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Integer id;
 
-    public String username;
+  public String username;
 
-    @NotEmpty
-    public String password;
+  @NotEmpty public String password;
 
-    @Column(unique = true)
-    public String email;
+  @Column(unique = true)
+  public String email;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "uploader")
-    private List<FileMetadata> fileMetadataList = new ArrayList<>();
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "uploader")
+  private List<FileMetadata> fileMetadataList = new ArrayList<>();
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "ownerFile")
-    private List<DownloadHistory> downloadDetailFiles = new ArrayList<>(); // Details of who has downloaded your files
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "ownerFile")
+  private List<DownloadHistory> downloadDetailFiles =
+      new ArrayList<>(); // Details of who has downloaded your files
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "downloader")
-    private List<DownloadHistory> yourDownloads = new ArrayList<>(); // Files you have downloaded
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "downloader")
+  private List<DownloadHistory> yourDownloads = new ArrayList<>(); // Files you have downloaded
+
+  @Override
+  public String toString() {
+    return "{"
+        + "\"id\":"
+        + id
+        + ", \"username\":\""
+        + username
+        + "\""
+        + ", \"password\":\""
+        + password
+        + "\""
+        + ", \"email\":\""
+        + email
+        + "\""
+        + "}";
+  }
 }
--- src/main/java/com/share_doc/sharing_document/services/DownloadHistoryService.java
+++ src/main/java/com/share_doc/sharing_document/services/DownloadHistoryService.java
@@ -1,3 +1,36 @@
 package com.share_doc.sharing_document.services;
 
-public class DownloadHistoryService {}
+import com.share_doc.sharing_document.entity.DownloadHistory;
+import com.share_doc.sharing_document.entity.FileMetadata;
+import com.share_doc.sharing_document.entity.User;
+import com.share_doc.sharing_document.repository.DownloadHistoryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DownloadHistoryService {
+  private final DownloadHistoryRepository downloadHistoryRepository;
+
+  public DownloadHistoryService(DownloadHistoryRepository downloadHistoryRepository) {
+    this.downloadHistoryRepository = downloadHistoryRepository;
+  }
+
+  public DownloadHistory addNew(DownloadHistory downloadHistory) {
+    return downloadHistoryRepository.save(downloadHistory);
+  }
+
+  public List<DownloadHistory> getByOwnerFile(User ownerFile) {
+    return downloadHistoryRepository.getDownloadHistoriesByOwnerFile(ownerFile);
+  }
+
+  public List<DownloadHistory> getByDownloader(User downloader) {
+    return downloadHistoryRepository.getDownloadHistoriesByDownloader(downloader);
+  }
+
+  public Optional<DownloadHistory> findByDocumentAndOwnerFileAndDownloader(
+      FileMetadata downloadedFile, User ownerFile, User downloader) {
+    return downloadHistoryRepository.findByDownloadedFileAndOwnerFileAndDownloader(
+        downloadedFile, ownerFile, downloader);
+  }
+}
